bf197ba2891189d368e94d34e1bc687eb64d7429
Docoppolis <j.parrott653@gmail.com>
2025-10-27 16:26:24 -0400

    Initial Commit

--- obj/Debug/net8.0/Web_Server.AssemblyInfo.cs
+++ obj/Debug/net8.0/Web_Server.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Web_Server")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+17db78a5ff8b193d7fb48ac2aa0a83e3066ebf90")]
 [assembly: System.Reflection.AssemblyProductAttribute("Web_Server")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Web_Server")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
